213afb266cbc3c10e064b9b1493510c196e7ac38
Dan Goldsmith <dan@d2g.org.uk>
2015-06-16 15:57:33 +0100

    We should honor the TTL

--- cache/memory/memory.go
+++ cache/memory/memory.go
@@ -16,7 +16,7 @@ type MemoryCacheRecord struct {
 
 func (this *Memory) Add(message *dns.Msg) error {
 	if message.Question[0].Qtype == dns.TypeA && message.Question[0].Qclass == dns.ClassINET {
-		this.Cache[message.Question[0].Name] = MemoryCacheRecord{Expiry: time.Now().Add(24 * time.Hour), Record: *message}
+		this.Cache[message.Question[0].Name] = MemoryCacheRecord{Expiry: time.Now().Add(time.Duration(message.Answer[0].Header().Ttl) * time.Second), Record: *message}
 	}
 	return nil
 }
